29a0a179da312fe0d5bd53cf8538fb9a7fd6d9c6
FLAVIO MOURA ROCHA <fmrocha@gmail.com>
2017-10-11 12:38:26 -0700

    Dashboard II Assignment first commit

--- dashboard/app/controllers/dojos_controller.rb
+++ dashboard/app/controllers/dojos_controller.rb
@@ -2,4 +2,24 @@ class DojosController < ApplicationController
 	def index
 		@dojos = Dojo.all
 	end
+
+	def new
+		@dojo = Dojo.new
+	end
+
+	def create
+		dojo = Dojo.new(dojo_params)
+		if dojo.save
+			redirect_to dojos_path(dojo)
+		else
+			redirect_to dojos_new_path
+		end
+	end
+
+
+	private
+	def dojo_params
+		params.require(:dojo).permit(:branch, :street, :city, :state)
+	end
+
 end
