67e7755589aa682eccc6f51642aecf514fcdaae2
FLAVIO MOURA ROCHA <fmrocha@gmail.com>
2017-10-13 09:50:20 -0700

    RGR TDD assignment first commit

--- project/project.rb
+++ project/project.rb
@@ -1,23 +1,13 @@
 
 class Project
   attr_accessor :name, :description, :owner
-  attr_reader :tasks
-  def initialize name, description, owner
+
+  def initialize name, desc
     @name = name
-    @description = description
-    @owner = owner
-    @tasks = []
+    @description = desc
   end
 
   def elevator_pitch
     "#{@name}, #{@description}"
-  end
-
-  def print_tasks
-    @tasks.each { |elem| puts elem }
-  end
-
-  def add_tasks task
-    @tasks << task
   end
 end
